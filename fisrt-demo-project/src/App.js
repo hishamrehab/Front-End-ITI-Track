8271c643be87fe8e00e4be269f40f1fbb4c11f0f
hishamrehab <hishamrehab30@gmail.com>
2026-01-25 22:21:10 +0200

    conditional rendering

--- fisrt-demo-project/src/App.js
+++ fisrt-demo-project/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./navbar/navbar.js"
+import Users from './users/Users.js';
 function App() {
   return (
     <div className="App">
      <h1>Hello From App</h1>
      <Navbar />
+     <Users />
     </div>
   );
 }
--- fisrt-demo-project/src/users/Users.js
+++ fisrt-demo-project/src/users/Users.js
@@ -1,9 +1,10 @@
 import React from "react";
-import Users from "./Users"
+
 export default class Users extends React.Component {
   constructor() {
     super();
     this.state = {
+        isAuth: true,
         users: [
             {name : "hend" , age : 20 , isAdmin : true},
             {name : "ali" , age : 22 , isAdmin : true},
@@ -12,19 +13,19 @@ export default class Users extends React.Component {
     }
   }
 
+
+
  render() {
     return (
         <>
+        <h1>Users</h1>
          <ul>
-            {this.state.users.map((user , index) => {
-                return (
-                    <>
-                    <li key={index}>{user.name}</li>
-                    <li key={index}>{user.age}</li>
-                    <li key={index}>{user.isAdmin}</li>
-                    </>
-                )
-            })}
+            {(this.state.isAuth)
+             ? this.state.users.map((user , index) => {
+               if(user.isAdmin){
+                 return (<li key={index}>{user.name}</li>)
+               }
+            }): <h1>Please Login First</h1>}
          </ul>
         </>
     )
