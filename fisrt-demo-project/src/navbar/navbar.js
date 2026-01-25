8e9a8c86bd3783723c49f8bb0b643e3f6381ee5d
hishamrehab <hishamrehab30@gmail.com>
2026-01-25 19:48:46 +0200

    class component

---
+++ fisrt-demo-project/src/navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+export default class Navbar extends React.Component {
+    constructor() {
+        super()
+    }
+  render() {
+    return(
+      <>
+        <h1>this is Navbar</h1>
+        <p>Hello</p>
+      </>
+    )
+  }
+}
