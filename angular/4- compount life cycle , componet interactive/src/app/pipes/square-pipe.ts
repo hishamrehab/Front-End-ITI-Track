81bdf5f60674cb47f82007214752bfb31c5d9a1a
hishamrehab <hishamrehab30@gmail.com>
2026-02-02 15:25:26 +0200

    Custom drictive ,pipe

---
+++ angular/4- compount life cycle , componet interactive/src/app/pipes/square-pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'square',
+})
+
+export class SquarePipe implements PipeTransform {
+  transform(value : number , power : number) : number {
+    return Math.pow(value,power);
+  }
+}
