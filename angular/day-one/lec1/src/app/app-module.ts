493b40d40e4d01cd7e8446e469c8b8b9a8c012a8
hishamrehab <hishamrehab30@gmail.com>
2026-04-12 15:17:21 +0200

    day one - lec one

---
+++ angular/day-one/lec1/src/app/app-module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [App]
+})
+
+export class AppModule { 
+  title = 'lec1';
+}
